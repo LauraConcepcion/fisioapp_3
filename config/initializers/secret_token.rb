07f8263d1a977e4275bc8a86b15c28cbb5bb2566
Laura Concepción Rodríguez <infousabi@gmail.com>
2012-09-03 14:38:00 +0100

    Primer commit, inicializamos proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fisioapp::Application.config.secret_token = '145bf6bdcf893ed2fd829eebcf17c2602bf296b2fdeca586440da77e4030b8d81c013c8180d2e43495b516e2609feaf75e212fc361191d8a3ba732427e08ca34'
